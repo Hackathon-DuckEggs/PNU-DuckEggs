259988d77e09deaa2f01c34894354725f84202a1
ByeongWooCho <whquddn55@gmail.com>
2021-07-25 23:24:45 +0900

    Update: 수정된 db구조에 대응
    Update: 로깅방식 변경
    Update: '/' 도메인 title 쿼리 and연산으로 변경
    Update: 코드 런타임 오류 그대로 리턴하는 문제 해결

--- backend/route/product.js
+++ backend/route/product.js
@@ -3,33 +3,54 @@ const router = express.Router()
 const { Product } = require('../model/Product')
 
 router.get('/:pCode', (req, res) => {
-	Product.findOne({'pCode': req.params.pCode}, (err, productInfo) => {
-		if (err) return res.status(400).json({success: false, err})
-		if (productInfo == null) return res.status(201).json({success: false, err: 'Undefined productCode'})
-		return res.status(200).json({success: true, productInfo})
-	})
+	try {
+		let today = new Date()
+		console.log(`[${today.getFullYear()}/${today.getMonth()}/${today.getDate()} ${today.getHours()}/${today.getMinutes()}/${today.getSeconds()}]${req.params.pCode}`)
+		Product.findOne({'pCode': req.params.pCode}, {_id: false}, (err, productInfo) => {
+			if (err) return res.status(400).json({success: false, err})
+			if (productInfo == null) return res.status(201).json({success: false, err: 'Undefined productCode'})
+			specs = {}
+			for (let key in productInfo['specs'])
+				specs[key.replace(/\[dot\]/, '.')] = productInfo['specs'][key]
+			productInfo['specs'] = specs
+			return res.status(200).json({success: true, productInfo})
+		})
+	} catch (err) {
+		console.log(err)
+		return res.status(400).json({success: false, err: 'Server is temporary down'})
+	}
 })
  
 router.get('/', (req, res) => {
-	var queryString = req.query.title
-	let width = req.query.width ? req.query.width : 130
-	let height = req.query.height ? req.query.height : 130
-	console.log(queryString, width, height)
-	if (queryString.length < 2)
-		return res.status(201).json({success: false, err: 'Query string is too short'})
+	try {
+		let title = req.query.title
+		let width = req.query.width ? req.query.width : 130
+		let height = req.query.height ? req.query.height : 130
+		let today = new Date()
+		console.log(`[${today.getFullYear()}/${today.getMonth()}/${today.getDate()} ${today.getHours()}/${today.getMinutes()}/${today.getSeconds()}]${title}, ${width}, ${height}`)
 
-	Product.find({$text: {$search: queryString, $caseSensitive: false}}, {title: true, pCode: true, _id: false} , (err, list) => {
-		if (err) return res.status(400).json({success: false, err})
-		result = list.map((item) => {
-			let t = {
-				pCode: item['pCode'],
-				title: item['title'],
-				image: `http://img.danawa.com/prod_img/500000/${item['pCode'].slice(item['pCode'].length - 3 , item['pCode'].length)}/${item['pCode'].slice(item['pCode'].length - 6, item['pCode'].length - 3)}/img/${item['pCode']}_1.jpg?shrink=${width}:${height}`
-			}
-			return t
-		})
-		return res.status(200).json({success: true, list: result})
-	}).limit(30)
+		if (title == null || title == '')
+			return res.status(201).json({success: false, err: 'Query string is empty'});
+		if (title.length < 2)
+			return res.status(201).json({success: false, err: 'Query string is too short'})
+
+		let queryString = "\"" + title.replace(/\s/g, "\" \"") + "\""
+		Product.find({$text: {$search: queryString, $caseSensitive: false}}, {title: true, pCode: true, _id: false} , (err, list) => {
+			if (err) return res.status(400).json({success: false, err})
+			result = list.map((item) => {
+				let t = {
+					pCode: item['pCode'],
+					title: item['title'],
+					image: `http://img.danawa.com/prod_img/500000/${item['pCode'].slice(item['pCode'].length - 3 , item['pCode'].length)}/${item['pCode'].slice(item['pCode'].length - 6, item['pCode'].length - 3)}/img/${item['pCode']}_1.jpg?shrink=${width}:${height}`
+				}
+				return t
+			})
+			return res.status(200).json({success: true, list: result})
+		}).limit(30)
+	} catch (err) {
+		console.log(err)
+		return res.status(400).json({success: false, err: 'Server is temporary down'})
+	}
 })
 
 module.exports = router;
